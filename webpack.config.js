0093c6d1e084365019eeeecb004c631d21093bbb
maxbarsukov <maximbarsukov@bk.ru>
2024-05-02 23:11:48 +0300

    fix: :bug: fix webpack config thar removed js code for jsf

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/main/webapp/resources/src/js/index.js',
   output: {
     filename: 'main.js',
